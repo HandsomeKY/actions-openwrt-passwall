dcf7f23eb84d688297f974362a8773c7442ee2ba
HandsomeYang <63038513+HandsomeKY@users.noreply.github.com>
2020-06-09 10:35:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,10 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/bin20088/luci-theme-argon-mc.git package/luci-theme-argon-mc
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/schen39/luci-app-serverchan package/luci-app-serverchan
+./scripts/feeds update -a
+./scripts/feeds install -a
